3ba6d9aeb83feef4ef05ff3e76a51e371174d4fb
avisek85 <Abhisheksinghrajput5888@gmail.com>
2024-10-29 00:54:15 +0530

    adding origin 2

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const limiter = rateLimit({
 
 const app = express();
 app.use(limiter);
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use('/api',routes);
 app.use('/api/auth',authRoutes);
@@ -26,12 +26,14 @@ app.use('/api/tasks',taskRoutes);
 
 
 app.use(cors({
-    origin: 'https://task-manager-two-woad.vercel.app', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allowed methods
-},{
-    origin: 'https://vercel.com/abhisheks-projects-1c338bd8/task-manager', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
+    origin: [
+        'https://task-manager-kappa-topaz.vercel.app',  // Current Vercel frontend URL
+        'https://task-manager-two-woad.vercel.app'      // Another Vercel URL if needed
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],  // Allowed methods
+    credentials: true  // If you need to handle cookies or authentication
 }));
+
 
 
 
